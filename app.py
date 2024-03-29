a7802e1e71d940082d16fd955382df7b74bfda4f
Sam Dickinson <sam.t.dickinson@gmail.com>
2024-03-29 18:38:52 +0000

    fixing typo in connection string

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(os.getenv("MONGODBD_URI"))
+    client = MongoClient(os.getenv("MONGODB_URL"))
     app.db = client.microblog
 
     @app.route("/", methods=["GET", "POST"])
